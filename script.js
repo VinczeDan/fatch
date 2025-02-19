ae83e1650d9a05a091e716a7d47660c74859c2a5
daniel.vincze15@gmail.com <daniel.vincze15@gmail.com>
2025-02-19 13:53:19 +0100

    fjkléas

--- script.js
+++ script.js
@@ -4,19 +4,20 @@ async function randomColorFetch() {
   return data.hex;
 }
 
-async function changeSquareColor() {
-  let color = await randomColorFetch();
-  document.querySelector(".szinezendo_doboz").style.backgroundColor = color;
-}
+async function changeSquareColors() {
+  let color1 = await randomColorFetch();
+  let color2 = await randomColorFetch();
+  let color3 = await randomColorFetch();
 
-function addEventListeners() {
-  document
-    .getElementById("changeColor")
-    .addEventListener("click", changeSquareColor);
+  document.querySelector(".szinezendo_doboz1").style.backgroundColor = color1;
+  document.querySelector(".szinezendo_doboz2").style.backgroundColor = color2;
+  document.querySelector(".szinezendo_doboz3").style.backgroundColor = color3;
 }
 
 function main() {
-  addEventListeners();
+  document
+    .getElementById("changeColor")
+    .addEventListener("click", changeSquareColors);
 }
 
 main();
